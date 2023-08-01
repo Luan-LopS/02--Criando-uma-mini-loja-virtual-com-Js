995c4afe41667aebb91a72596ded94b7f77c681d
Luan <luam10@hotmail.com.br>
2023-08-01 10:54:20 -0300

    consumo api

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const items = [{
+/*const items = [{
     id: 0,
     nome: 'camiseta',
     img: 'https://images.tcdn.com.br/img/img_prod/275189/camisa_azul_royal_100_poliester_para_sublimacao_m_2701_1_20200722153204.jpg',
@@ -13,44 +13,56 @@ const items = [{
     nome: 'tenis',
     img: 'https://images.tcdn.com.br/img/img_prod/275189/camisa_azul_royal_100_poliester_para_sublimacao_m_2701_1_20200722153204.jpg',
     quantidade: 0,
-}]
+}]*/
+//item de teste
 
 
-iniciarLoja = ()=>{
+//criação da loja
+startShop = (items) => {
+    console.log(items)
     var containerProdutos = document.getElementById("produtos")
-    items.map((val)=>{
-        containerProdutos.innerHTML+=`
-        <div class"produtos-single>
-        <img src="${val.img}" width="100px">
-        <p>${val.nome}</P>
+    items.map((val) => {
+        containerProdutos.innerHTML += `
+        <div class"produtos-single">
+        <img src="${val.image}" width="100px">
+        <p>${val.title}</P>
+        <p>$ ${val.price}</p>
         <a key="${val.id}" href="#">adicionar ao carrinho!</a>
         </div>        
         `
     })
 }
 
-iniciarLoja()
+async function getApi() {
+    const items = await fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+    startShop(items)
+    updateCart(items)
+}
 
-atualizarCarrinho = ()=>{
+getApi()
+
+updateCart = (items) => {
     var containerCarrinho = document.getElementById("carrinho")
-    containerCarrinho.innerHTML=""
-    items.map((val)=>{
-        if(val.quantidade!=0){
-        containerCarrinho.innerHTML+=`
-        <p>${val.nome}| quantidade: ${val.quantidade}</P>
-        <hr>
-        `
+    containerCarrinho.innerHTML = ""
+    items.map((val) => {
+        if (val.quantidade === 0 || val.quantidade<0) {
+           let quantidade = 
+            containerCarrinho.innerHTML += `
+            <p>${val.title}| quantidade: ${val.quantidade}</P>
+            <hr>
+            `
         }
     })
 }
 
 var links = document.getElementsByTagName("a")
 
-for(var i = 0; i<links.length;i++){
-    links[i].addEventListener("click",function(){
-            let key = this.getAttribute("key")
-            items[key].quantidade++
-            atualizarCarrinho()
-            return false
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function () {
+        let key = this.getAttribute("key")
+        items[key].quantidade++
+        updateCart()
+        return false
     })
 }
